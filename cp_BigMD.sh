38ae43b73ef4e3603a5c88d14cbf59554b092bd3
xiaodongli1986 <xiaodongli@kias.re.kr>
2019-03-26 16:57:32 +0800

    upload many things

--- cp_BigMD.sh
+++ cp_BigMD.sh
@@ -1,1 +1,3 @@
 rsync -avrP /public/projects/cf_pks/BigMDPL_mge1e13/*.2pcf BigMD_2pcfs/
+rsync -avrP /public/projects/cf_pks/BigMDPL_mge1e13/test/*.2pcf BigMD_2pcfs/
+rsync -avrP /public/projects/cf_pks/BigMD/z0/*.2pcf BigMD_2pcfs/
