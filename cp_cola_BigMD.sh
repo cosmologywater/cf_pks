ef104941497a45ca435ea657fb3e83d2614b604a
xiaodongli1986 <xiaodongli@kias.re.kr>
2019-05-16 15:28:14 +0800

    upload many things

--- cp_cola_BigMD.sh
+++ cp_cola_BigMD.sh
@@ -3,7 +3,7 @@ rsync -avrP /public/projects/simulation/cola_output_600par_512box_BigMD/0_30/*.2
 rsync -avrP /public/projects/simulation/cola_output_600par_512box_BigMD/31_60/*.2pcf ./cola_2pcfs/cola_output_600par_512box_BigMD/
 rsync -avrP /public/projects/simulation/cola_output_600par_512box_BigMD/61_90/*.2pcf ./cola_2pcfs/cola_output_600par_512box_BigMD/
 rsync -avrP /public/projects/simulation/cola_output_600par_512box_BigMD/91_120/*.2pcf ./cola_2pcfs/cola_output_600par_512box_BigMD/
-
+rsync -avrP /public/projects/simulation/cola_output_600par_512box_BigMD/121_150/*.2pcf ./cola_2pcfs/cola_output_600par_512box_BigMD/
 
 
 
